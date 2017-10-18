bb5ac2768f649fd9098be599ffc1fcf499c818d1
Anwar Montasir <anwarmontasir@gmail.com>
2017-10-17 22:26:41 -0700

    begin postcss prep

--- webpack.config.js
+++ webpack.config.js
@@ -15,11 +15,28 @@ const dest = join(root, 'dist');
 
 const isDev = NODE_ENV === 'development';
 
+const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
+
+const matchCssLoaders = /(^|!)(css-loader)($|!)/;
+
+const findLoader = (loaders, match) => {
+    const found = loaders.filter(l => l && l.loader && l.loader.match(match));
+    return found ? found[0] : null;
+}
+// existing css loader
+const cssloader = findLoader(config.module.loaders, matchCssLoaders)
+
 var config = getConfig({
     isDev: isDev,
     in: join(__dirname, 'src/app.js'),
     out: dest,
     clearBeforeBuild: true
 });
+
+config.postcss = [].concat([
+    require('precss')({}),
+    require('autoprefixer')({}),
+    require('cssnano')({})
+]);
 
 module.exports = config;
