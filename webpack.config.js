781864da375ba0c45ebf456d73e98f2266a13e0b
Anwar Montasir <anwarmontasir@gmail.com>
2017-10-17 20:27:39 -0700

    modify webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
 const webpack = require('webpack');
-const fs      = require('fs');
-const path    = require('path'),
-      join    = path.join,
-      resolve = path.resolve;
+const fs = require('fs');
+const path = require('path');
+const join = path.join;
+const resolve = path.resolve;
 
 const getConfig = require('hjs-webpack');
+
+const root = resolve(__dirname);
+const src = join(root, 'src');
+const modules = join(root, 'node-modules');
+const dest = join(root, 'dist');
+
+var config = getConfig({
+    in: join(__dirname, 'src/app.js'),
+    out: dest,
+    clearBeforeBuild: true
+})
